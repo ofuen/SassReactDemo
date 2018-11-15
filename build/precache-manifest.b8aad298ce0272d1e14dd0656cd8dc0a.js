6a2fb57ed829ca8e2bfa8daa90de4b1aaf844022
oscar <oscar@o2webdev.com>
2018-11-14 20:00:50 -0500

    upload

---
+++ build/precache-manifest.b8aad298ce0272d1e14dd0656cd8dc0a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "68321529bd806f2cf1ad",
+    "url": "/SassReactDemo/static/js/runtime~main.68321529.js"
+  },
+  {
+    "revision": "9703b2b58e87219d14d9",
+    "url": "/SassReactDemo/static/js/main.9703b2b5.chunk.js"
+  },
+  {
+    "revision": "c2ea6c283125f00601cc",
+    "url": "/SassReactDemo/static/js/1.c2ea6c28.chunk.js"
+  },
+  {
+    "revision": "9703b2b58e87219d14d9",
+    "url": "/SassReactDemo/static/css/main.57802a94.chunk.css"
+  },
+  {
+    "revision": "2473228002636f82eed294609757ca00",
+    "url": "/SassReactDemo/index.html"
+  }
+];
